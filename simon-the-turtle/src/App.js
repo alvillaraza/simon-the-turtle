d0d061bd9f5727370cd942797553ce20d901a3a1
Alexis <alexis.borja@gmail.com>
2022-07-17 09:55:05 -0700

    text and svg added

--- simon-the-turtle/src/App.js
+++ simon-the-turtle/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import instagram from "./assets/instagram.svg";
+import turtle_logo_white from "./assets/turtle_logo_white.svg"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={instagram} alt="instagram logo" />
+        <img src={turtle_logo_white} alt="white simon the turtle logo" />
         <p>
-          Simon the Turtle
+          After building HeadSpace in 2016, I decided to build a smaller, more
+          portable vehicle to be able to transport it around to different events
+          more easily. Working with an amazing team of friends from Phoenix, AZ,
+          we built an electric, remote-controlled turtle: SIMON the TURTLE.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Two years later, we’ve refined the design so that it can completely
+          disassemble into the bed of a pickup truck.
+        </p>
+        <p>
+          SIMON the TURTLE is a Sensory Immersive Mobile Observation Nest the
+          Transforming Universal Remote-controlled Load-bearing Electric
+          vehicle.
+        </p>
       </header>
     </div>
   );
