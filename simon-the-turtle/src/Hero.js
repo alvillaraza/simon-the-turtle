c0fd6a683203487b559615b1995e0dc4caa4b2f7
Alexis <alexis.borja@gmail.com>
2022-07-17 19:17:21 -0700

    added hero-copy

--- simon-the-turtle/src/Hero.js
+++ simon-the-turtle/src/Hero.js
@@ -1,10 +1,13 @@
 import hero_image from './assets/hero_image.png'
 import hero_blur from "./assets/hero_blur.png";
+import hero_copy from "./assets/hero_copy.svg";
+
 import hero_image_title from "./assets/hero_image_title.jpeg";
 
 const Hero = () => {
   return (
     <section className='hero-image'>
+      <img src={hero_copy} className='hero-copy' alt='simon the turtle' />
       <img src={hero_image} className='hero-image-front' alt='hero' />
       <img src={hero_blur} alt="blurry hero" />
       {/* <img src={hero_image_title} alt="simon the turtle" /> */}
